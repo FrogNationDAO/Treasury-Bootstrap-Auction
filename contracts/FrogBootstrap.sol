2f7692216401461ec795b4903e90549ce39df860
plinki <geolazaria@gmail.com>
2021-11-07 20:04:00 +0200

    base data structure

--- contracts/FrogBootstrap.sol
+++ contracts/FrogBootstrap.sol
@@ -1,8 +1,54 @@
 pragma solidity ^0.8.7;
 
 import "./ERC721/CustomERC721Metadata.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract FrogBootstrap is CustomERC721Metadata {
+contract FrogBootstrap is CustomERC721Metadata, Ownable {
+    using SafeMath for uint256;
+
+    struct Project {
+        string name;
+        string artist;
+        string description;
+        string website;
+        string license;
+        bool dynamic;
+        string projectBaseURI;
+        string projectBaseIpfsURI;
+        uint256 invocations;
+        uint256 maxInvocations;
+        string ipfsHash;
+        bool useHashString;
+        bool useIpfs;
+        bool active;
+        bool locked;
+        bool paused;
+    }
+
+    uint256 public frogBootstrapPercentage = 20;
+    uint256 public nextProjectId = 0;
+
+    mapping(uint256 => Project) projects;
+
+    mapping(uint256 => address) public projectIdToArtistAddress;
+    mapping(uint256 => string) public projectIdToCurrencySymbol;
+    mapping(uint256 => address) public projectIdToCurrencyAddress;
+    mapping(uint256 => uint256) public projectIdToPricePerTokenInWei;
+    mapping(uint256 => address) public projectIdToAdditionalPayee;
+    mapping(uint256 => uint256) public projectIdToAdditionalPayeePercentage;
+    mapping(uint256 => uint256) public projectIdToSecondaryMarketRoyaltyPercentage;
+
+
+    mapping(uint256 => string) public staticIpfsImageLink;
+    mapping(uint256 => uint256) public tokenIdToProjectId;
+    mapping(uint256 => uint256[]) internal projectIdToTokenIds;
+    mapping(uint256 => bytes32) public tokenIdToHash;
+    mapping(bytes32 => uint256) public hashToTokenId;
+
+    mapping(address => bool) public isWhitelisted;
+    mapping(address => bool) public isMintWhitelisted;
+
 
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) CustomERC721Metadata(_name, _symbol) {
